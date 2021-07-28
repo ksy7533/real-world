da064d015317bbb7f00e97c4d5a1db7fa8fd66a9
victor-v <victor.v@kakaomobility.com>
2021-07-28 18:26:44 +0900

    기본 라우팅 추가, NoMatch url 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './page/Home'
+import NoMatch from './page/NoMatch'
+
 import './App.css'
 
-function App() {
-  return <div>test</div>
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='*'>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
